092d637be91cf58e77ac422b47af030e7377855d
Kai Pham <kkai@vinova.sg>
2018-04-20 16:42:32 +0700

    init project

---
+++ DemoAnimation/DemoAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoAnimation
+//
+//  Created by Kai Pham on 4/20/18.
+//  Copyright © 2018 Kai Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
